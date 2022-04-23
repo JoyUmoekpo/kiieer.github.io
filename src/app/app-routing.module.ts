17c58ec659b8f83339f911e64ee03db60364c023
kiieer <christliebkiersten@gmail.com>
2022-04-23 10:50:34 -0400

    routing wip

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RoomserviceComponent } from './roomservice/roomservice.component';
+import { ReservationsComponent } from './reservations/reservations.component';
+import { AttractionsComponent } from './attractions/attractions.component';
+
+const routes:Routes=[
+  {path:'login', component:LoginComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'roomservice', component: RoomserviceComponent},
+  {path: 'reservations', component: ReservationsComponent},
+  {path: 'attractions', component: AttractionsComponent},
+  { path: '**', redirectTo: '/home', pathMatch: 'full' },];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RoomserviceComponent } from './roomservice/roomservice.component';
+import { ReservationsComponent } from './reservations/reservations.component';
+import { AttractionsComponent } from './attractions/attractions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    RoomserviceComponent,
+    ReservationsComponent,
+    AttractionsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
