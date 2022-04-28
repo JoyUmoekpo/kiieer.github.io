10b4ce031ad9de4a11ece2f0296b055fca654a7e
Joy Umoekpo <joyumoekpo1@gmail.com>
2022-04-27 23:04:26 -0500

    Added MatModule from angular/material and added references

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,21 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from'@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {MatDialogModule} from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, MatButtonModule, MatToolbarModule, MatInputModule, MatDialogModule, MatTableModule, MatIconModule,
+    MatMenuModule, MatFormFieldModule, MatCardModule],
+  exports: [CommonModule, MatButtonModule, MatToolbarModule, MatInputModule, MatDialogModule, MatTableModule, MatIconModule,
+    MatMenuModule, MatFormFieldModule, MatCardModule],
 })
-export class MaterialModule { }
+export class CustomMaterialModule { }
